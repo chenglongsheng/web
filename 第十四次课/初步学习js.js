b61b43f60025f8b5792a39151851a4e64ccfe1ae
cls688 <1536463948@qq.com>
2020-11-04 10:01:13 +0800

    初步学习JS

---
+++ 第十四次课/初步学习js.js
@@ -0,0 +1,8 @@
+function orange(){
+    var array=[123,32,32,43,43,21,2324,21]
+    for(let index =0;index<array.length;index++){
+        const element = array[index];
+        console.log(element);
+    }
+}
+orange();
