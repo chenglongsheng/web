d6f30492150c58ac7ae1cfc73cf8e2c32a2d3d14
cls688 <1536463948@qq.com>
2020-12-09 09:05:07 +0800

    立即执行函数

--- 第二十三次课/立即执行函数.js
+++ 第二十三次课/立即执行函数.js
@@ -1,15 +1,16 @@
 ; (function (x) {
     console.log("this is chestnut")
-    return ((x) => function () {
+    return (function (x) {
         console.log("this is haw")
+        x()
         x.printName = function () {
-            console.log("this is x().printName")
+            console.log("this is x.printName")
             return this.name
         }
         return 200 + x.printName() + x()
-    }())
-        (() => {
-            this.name = 6666; console.log("this is pitaya");
-            return 3999
-        })
+    })(() => {
+        this.name = 6666
+        console.log("this is pitaya")
+        return 3999
+    })
 })()
